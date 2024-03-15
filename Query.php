c9e14708b3de562d9d7e697739e363b31d401101
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-03-15 16:51:42 +0545

    Updated query class to handle select and table function.

--- Query.php
+++ Query.php
@@ -4,4 +4,19 @@ namespace NGFramer\NGFramerPHPSQLBuilder
 
 Class Query
 {
+    // Variable to carry the name of the table.
+    $this->tableName = null;
+
+    // Function to get and set the tableName;
+    public function table(string $tableName)
+    {
+        \NGFramer\NGFramerPHPSQLBuilder\datamanipulation\Table::setTableName($tableName);
+        return \NGFramer\NGFramerPHPSQLBuilder\datamanipulation\Table::getTableName();
+    }
+
+    public function select(string ...$fields = null)
+    {
+        $tableName = $this->tableName;
+        \NGFramer\NGFramerPHPSQLBuilder\datamanipulation\Select::build($tableName, $fields = null)
+    }
 }
