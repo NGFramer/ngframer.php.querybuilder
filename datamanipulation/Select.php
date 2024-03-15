4b5d10f3a8b52bbe5e532cd1845a1edbbec47abb
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-03-15 11:57:45 +0545

    Updated select function to accept table name.

--- datamanipulation/Select.php
+++ datamanipulation/Select.php
@@ -4,12 +4,12 @@ namespace NGFramer\NGFramerPHPSQLBuilder\datamanipulation;
 
 Class Select
 {
-    public static function build(string ...$fields = null)
+    public static function build(string $tableName, string ...$fields = null)
     {
-        if (empty($fields)) {
+        if (empty($fields) || (count($fields) == 1 && $fields[0] = '*') {
             return "SELECT *";
         } else {
-            return "SELECT " . implode(', ', $fields);
+            return "SELECT " . implode(', ', $fields) . "FROM " . $tableName;
         }
     }
 }
