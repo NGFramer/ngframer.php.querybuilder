bdbb9f8bff31d89e309ccca59fcc50580fcaeab7
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-03-17 17:59:02 +0545

    Fixed select function to take in array.
    Fixed the select build function to accept array. The function won't be used directly, will need be used form the main (Query) class.

--- datamanipulation/Select.php
+++ datamanipulation/Select.php
@@ -4,9 +4,16 @@ namespace NGFramer\NGFramerPHPSQLBuilder\datamanipulation;
 
 Class Select
 {
-    public static function build(string $tableName, string ...$fields = null)
+    public static function build(string $tableName, array $fields = []): string
     {
-        if (empty($fields) || (count($fields) == 1 && $fields[0] = '*') {
+        // Check if the $fields array's element is string.
+        foreach ($fields as $field) {
+            if (!is_string($field)) {
+                throw new \InvalidArgumentException('Field names must be strings.');
+            }
+        }
+        // Check if the $fields array is empty or not.
+        if (empty($fields) || (count($fields) == 1 && $fields[0] = '*')) {
             return "SELECT *";
         } else {
             return "SELECT " . implode(', ', $fields) . "FROM " . $tableName;
