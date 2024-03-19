88e1b3cc1eafd4284787594e4154accd1352823d
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-03-19 08:34:19 +0545

    Creation of Table class.

--- Query.php
+++ Query.php
@@ -4,8 +4,9 @@ namespace NGFramer\NGFramerPHPSQLBuilder;
 
 Class Query
 {
-    // Variable to carry the name of the table.
-    private $tableName = null;
+    // Variable defined here.
+    private string $tableName = null;
+    private array $bindParameters = [];
 
     // Function to get and set the tableName;
     public function table(string $tableName): void
@@ -18,5 +19,20 @@ Class Query
     {
         $tableName = $this->tableName;
         \NGFramer\NGFramerPHPSQLBuilder\datamanipulation\Select::build($tableName, $fields);
+    }
+
+    // More utilities functions for the class.
+    public function accessbindParametersIndex(): int
+    {
+        // Returns the number of binding parameters that have been stored.
+        return count($this->bindParameters);
+    }
+
+    public function updateBindParameters(string $key, string $value){
+        if (array_key_exists($key, $this->bindParameters)){
+            throw new Exception("Something unexpected happened. Repeated bindParameters Key.");
+        }else{
+            $this->bindParameters[$key] = $value;
+        }
     }
 }
