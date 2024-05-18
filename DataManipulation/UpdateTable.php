5ba2443ce7536b8c66958e1116c848631b33aa69
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-05-18 20:37:00 +0545

    Update func fixed and build func added.

--- DataManipulation/UpdateTable.php
+++ DataManipulation/UpdateTable.php
@@ -6,12 +6,16 @@ use NGFramer\NGFramerPHPSQLBuilder\DataManipulation\Supportive\_DmlTable;
 
 class UpdateTable extends _DmlTable
 {
+    // Variables to be used in the class.
+    private int $counter = 0;
+
+
+
+
     // Use the following trait to access the functions.
-    use WhereTrait{
+    use WhereTrait {
         WhereTrait::build as whereBuild;
     }
-
-
 
 
     // Constructor for the UpdateTable class.
@@ -26,24 +30,31 @@ class UpdateTable extends _DmlTable
     }
 
 
-
-
-    // Update function for the class.
     public function update(array $data): void
     {
         // Check if the data is an associative array.
         if ($this->isAssocArray($data)) {
+            // Single update
             foreach ($data as $key => $value) {
-                $this->addToQueryLogDeep('data', $key, $value);
+                $this->addToQueryLogDeep('data', $key, 'field', $key);
+                $this->addToQueryLogDeep('data', $key, 'value', $value);
             }
         } else {
-            foreach ($data as $value) {
-                $this->addToQueryLogDeepArray('data', $value);
+            // Multiple updates
+            foreach ($data as $element) {
+                if (is_array($element) && $this->isAssocArray($element)) {
+                    $this->addToQueryLogDeep('data', $this->counter, 'field', $element['field']);
+                    $this->addToQueryLogDeep('data', $this->counter, 'value', $element['value']);
+                } else if (is_array($element) && !$this->isAssocArray($element)) {
+                    $this->addToQueryLogDeep('data', $this->counter, 'field', $element[0]);
+                    $this->addToQueryLogDeep('data', $this->counter, 'value', $element[1]);
+                } else {
+                    throw new \InvalidArgumentException('Data must be an array/s.');
+                }
+                $this->counter++;
             }
         }
     }
-
-
 
 
     // Go direct function for where conditions.
@@ -56,11 +67,44 @@ class UpdateTable extends _DmlTable
     }
 
 
-
-
     // Builder function for the class.
-    public function build(bool $goDirect = false, int $bindIndexStarter = 0): string
+    public function build(): string
     {
-        return "";
+        // Get the queryLog initially to process.
+        $queryLog = $this->getQueryLog();
+
+        // Start building the update query.
+        $query = 'UPDATE ' . $queryLog['table'] . ' SET ';
+
+        // Build the SET clause.
+        $setData = [];
+
+        // Check if the data is empty.
+        if (empty($queryLog['data'])) {
+            throw new \InvalidArgumentException('No data to update.');
+        }
+
+        // Loop through the data to find the values to update.
+        foreach ($queryLog['data'] as $elements) {
+            $key = $elements['field'] ?? $elements[0];
+            $value = $elements['value'] ?? $elements[1];
+            // Check method of execution and process the data accordingly.
+            // For the default method, prepared statements.
+            if (!$this->isGoDirect()) {
+                $bindIndex = $this->getBindIndexStarter();
+                $this->updateBindParameters($key.$bindIndex, $value);
+                $setData[] = "$key = :$key$bindIndex";
+            } // For the direct method, add the values directly.
+            else {
+                $value = $this->sanitizeValue($value);
+                $setData[] = "$key = '$value'";
+            }
+        }
+        // Add the SET clause to the query.
+        $query .= implode(', ', $setData);
+        // Add the WHERE clause if present.
+        $query .= $this->whereBuild();
+        // Return the query.
+        return $query;
     }
 }
