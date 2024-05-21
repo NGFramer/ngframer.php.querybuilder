f05e1510303a2c00d7078dc491d3ed0d6683e45d
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-05-21 22:15:13 +0545

    CreateTable can be built with build() func.
    Completed the development of build function for CreateTable class.

--- DataDefinition/CreateTable.php
+++ DataDefinition/CreateTable.php
@@ -2,6 +2,7 @@
 
 namespace NGFramer\NGFramerPHPSQLBuilder\DataDefinition;
 
+use NGFramer\NGFramerPHPSQLBuilder\DataDefinition\Supportive\_DdlDefault;
 use NGFramer\NGFramerPHPSQLBuilder\DataDefinition\Supportive\_DdlTableColumn;
 
 class CreateTable extends _DdlTableColumn
@@ -17,14 +18,12 @@ class CreateTable extends _DdlTableColumn
     }
 
 
-
-
     // Function related to the addition of columns.
     // Logic behind the addition of column:
     // 1. Find the index of the column.
     // 2. Add the column to the query log = [ 'columns' => [ 'column' => $columnName ] ]
     // 3. The third step is to select the query for the addition of more attributes to the column.
-    public function addColumn($columnName):self
+    public function addColumn($columnName): self
     {
         // Make modification to the query log, we have added the table name and table's action previously.
         // We find the index for the column, and add the column, and it's name there.
@@ -38,13 +37,68 @@ class CreateTable extends _DdlTableColumn
     }
 
 
-
-
     // Builder function for the class.
     public function build(): string
     {
-        return "CREATE TABLE " . $this->getTableName();
-        // TODO: Implement build() method.
+        $tableName = $this->getQueryLog()['table'];
+        // Initialize the final creation query.
+        $query = "CREATE TABLE IF NOT EXISTS `$tableName` (";
+        // Turn for processing the columns query.
+        $columns = $this->getQueryLog()['columns'];
+        // Check if the $column is set or not and empty or not.
+        if (empty($columns)) {
+            throw new \Exception('No columns found to create table');
+        }
+        // Loop through the columns and build the column query.
+        foreach ($columns as $column) {
+            $query .= $this->buildColSql($column) . ', ';
+        }
+        // Remove the last comma from the query.
+        $query = rtrim($query, ', ');
+        // Close the query with the closing bracket.
+        $query .= ')';
+        // Return the finalized query.
+        return $query;
     }
 
+
+    // Helper function for build to build column attribute.
+    public function buildColSql(array $columnDefinition): string
+    {
+        if (!isset($columnDefinition['column'])) {
+            throw new \Exception('Column name is required');
+        }
+        if (!isset($columnDefinition['type'])) {
+            throw new \Exception('Column type for ' . $columnDefinition['column'] . ' is required');
+        }
+        if (!isset($columnDefinition['length'])) {
+            $columnDefinition['length'] = _DdlDefault::getLength($columnDefinition['type']);
+        }
+        // Main column definition query.
+        $columnSql = '`' . $columnDefinition['column']. '`' . ' ' . $columnDefinition['type'] . '(' . $columnDefinition['length'] . ')';
+        // Check for what attribute comes when at order.
+        if (isset($columnDefinition['null']) && $columnDefinition['null'] === true) {
+            $columnSql .= 'NULL';
+        }
+        if (isset($columnDefinition['null']) && $columnDefinition['null'] === false) {
+            $columnSql .= ' NOT NULL';
+        }
+        if (isset($columnDefinition['unique']) && $columnDefinition['unique']) {
+            $columnSql .= ' UNIQUE';
+        }
+        if (isset($columnDefinition['primary']) && $columnDefinition['primary']) {
+            $columnSql .= ' PRIMARY KEY';
+        }
+        if (isset($columnDefinition['autoIncrement']) && $columnDefinition['autoIncrement']) {
+            $columnSql .= ' AUTO_INCREMENT';
+        }
+        if (isset($columnDefinition['default'])) {
+            $columnSql .= ' DEFAULT ' . $columnDefinition['default'];
+        }
+        if (isset($columnDefinition['foreign'])) {
+            $columnSql .= ' FOREIGN KEY (' . $columnDefinition['column'] . ') REFERENCES ' . $columnDefinition['foreign']['table'] . '(' . $columnDefinition['foreign']['column'] . ')';
+        }
+        // Return the finalized column query.
+        return $columnSql;
+    }
 }
