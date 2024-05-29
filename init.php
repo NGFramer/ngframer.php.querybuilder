8e0c2050f494a0c4630a2bed9590c333260a4715
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-05-29 15:09:36 +0545

    Updated init.php to convert error to exception.
    Modified the following file:
    1. /init.php

--- init.php
+++ init.php
@@ -1,8 +1,11 @@
 <?php
 namespace MyLibrary;
 
-use NGFramer\NGFramerPHPException\exception\SqlBuilderException;
+use NGFramer\NGFramerPHPException\exception\SqlBuilderError;
 use NGFramer\NGFramerPHPException\handler\SqlBuilderExceptionHandler;
 
-// Set the custom exception handler for the library
+
+//Convert the error to an exception (SqlBuilderException).
+set_error_handler([SqlBuilderError::class, 'errorToException']);
+// Set the custom exception handler for the library.
 set_exception_handler([SqlBuilderExceptionHandler::class, 'handle']);
