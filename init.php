0e878962690e86727839ab64705b6f8cf9bf5eb8
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-06-02 11:46:37 +0545

    Fixed error to exception function name.

--- init.php
+++ init.php
@@ -6,6 +6,6 @@ use NGFramer\NGFramerPHPException\handler\SqlBuilderExceptionHandler;
 
 
 //Convert the error to an exception (SqlBuilderException).
-set_error_handler([SqlBuilderError::class, 'errorToException']);
+set_error_handler([SqlBuilderError::class, 'convertToException']);
 // Set the custom exception handler for the library.
 set_exception_handler([SqlBuilderExceptionHandler::class, 'handle']);
