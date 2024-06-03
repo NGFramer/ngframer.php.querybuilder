886d8970130391b2864858a9ea075bf1b0d93aae
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-06-03 12:28:25 +0545

    Changed to static call for Query generation starter.

--- Query.php
+++ Query.php
@@ -7,12 +7,12 @@ require 'vendor/autoload.php';
 Class Query
 {
     // Function to get and set the tableName and viewName;
-    public function table(string $tableName): Table
+    public static function table(string $tableName): Table
     {
         return new Table($tableName);
     }
 
-    public function view(string $viewName): View
+    public static function view(string $viewName): View
     {
         return new View($viewName);
     }
