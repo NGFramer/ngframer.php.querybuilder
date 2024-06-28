338ef753774c4702d6079df1859118e82d0ef23a
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-28 13:16:37 +0545

    Updated Query to include start/stop.

--- Query.php
+++ Query.php
@@ -2,14 +2,22 @@
 
 namespace NGFramer\NGFramerPHPSQLServices;
 
+use NGFramer\NGFramerPHPSQLServices\TransactionControl\StartStop;
+
 Class Query
 {
-    // Function to get and set the tableName and viewName;
+    // Use the following trait.
+    use StartStop;
+
+
+    // Function to get and set the tableName.
     public static function table(string $tableName): Table
     {
         return new Table($tableName);
     }
 
+
+    // Function to get and set the viewName.
     public static function view(string $viewName): View
     {
         return new View($viewName);
