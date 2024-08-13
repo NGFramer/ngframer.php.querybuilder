5ffe52a9de355339be91738e15d7a5b4b83f223f
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-08-13 20:53:04 +0545

    Created DropView builder class.

---
+++ Processes/BuildQuery/DataDefinition/DropView.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace NGFramer\NGFramerPHPSQLServices\Processes\BuildQuery\DataDefinition;
+
+use Exception;
+
+/**
+ * Let's say this is the duplicate of the class DropTable.
+ */
+
+class DropView
+{
+    /**
+     * Variable to store the actionLog.
+     * @var array|null
+     */
+    private ?array $actionLog;
+
+
+    /**
+     * Constructor function.
+     * @param array $actionLog
+     */
+    public function __construct(array $actionLog)
+    {
+        $this->actionLog = $actionLog;
+    }
+
+
+    /**
+     * This function builds the query from the action log.
+     * @return array
+     * @throws Exception
+     */
+    public function build(): array
+    {
+        // Get the data required.
+        $actionLog = $this->actionLog;
+        $viewName = $actionLog['view'];
+
+        // Start making the query (initial part).
+        $query = "DROP VIEW `$viewName`";
+
+        // Return the query built.
+        return ['query' => $query];
+    }
+}
