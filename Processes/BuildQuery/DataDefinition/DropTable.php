8f682a0f3eddfedf5435c75f46300611ad66808c
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-08-13 20:40:21 +0545

    Created DropTable class.

---
+++ Processes/BuildQuery/DataDefinition/DropTable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace NGFramer\NGFramerPHPSQLServices\Processes\BuildQuery\DataDefinition;
+
+use Exception;
+
+class DropTable
+{
+    /**
+     * Variable to store the actionLog.
+     * @var array|null
+     */
+    private ?array $actionLog;
+
+
+    /**
+     * Constructor function.
+     * @param array $actionLog
+     */
+    public function __construct(array $actionLog)
+    {
+        $this->actionLog = $actionLog;
+    }
+
+
+    /**
+     * This function builds the query from the action log.
+     * @return array
+     * @throws Exception
+     */
+    public function build(): array
+    {
+        // Get the data required.
+        $actionLog = $this->actionLog;
+        $tableName = $actionLog['table'];
+
+        // Start making the query (initial part).
+        $query = "DROP TABLE `$tableName`";
+
+        // Return the query built.
+        return ['query' => $query];
+    }
+}
