22158014d8bf16c95cd0c9661a083f70e3f04277
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-08-15 11:30:46 +0545

    Created SelectTable builder class (firstDraft).

---
+++ Processes/BuildQuery/DataManipulation/SelectTable.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace NGFramer\NGFramerPHPSQLServices\Processes\BuildQuery\DataManipulation;
+
+class SelectTable
+{
+    /**
+     * Array to store the actionLog.
+     * @var array|null
+     */
+    private ?array $actionLog;
+
+
+    /**
+     * Function that updates the actionLog.
+     * @param array $actionLog
+     * @return $this
+     */
+    public function __construct(array $actionLog)
+    {
+        $this->actionLog = $actionLog;
+        return $this;
+    }
+
+
+    /**
+     * This function builds the select query from the actionLog.
+     * @return string[]
+     */
+    public function build(): array
+    {
+        // Get the actionLog.
+        $actionLog = $this->actionLog;
+        // Get the fields to capture (select).
+        $columns = $actionLog['columns'];
+        // Get the table to select from.
+        $table = $actionLog['table'];
+
+        // Start building the query.
+        $query = 'SELECT ' . implode(', ', $columns) . ' FROM ' . $table;
+
+        // TODO: Add logic to build the limit, where, sortBy, groupBy, etc.
+
+        // Return query.
+        return ['query' => $query];
+    }
+}
