58592919c1c416c8d0a656bbaa021c0523ba0949
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-08-18 18:26:36 +0545

    Updated update logger with comments.

--- Actions/DataManipulation/UpdateTable.php
+++ Actions/DataManipulation/UpdateTable.php
@@ -29,6 +29,11 @@ final class UpdateTable extends StructureTable
      */
     public function update(array $updateData): void
     {
+        // Check if the updateData is on the following formats.
+        // method01 = update(['column', 'value', 'type'], ['column', 'value', 'type'], ['column', 'value', 'type'], ...);
+        // method02 = update(['column' => ['value', 'type']], ['column' => ['value', 'type']], ['column' => ['value', 'type']], ...);
+        // method03 = update(['column' => 'value', 'column' => 'value', 'column' => 'value', ...])
+
         // Check if updateData is empty.
         if (empty($updateData)) {
             throw new Exception('No data passed to update.');
@@ -39,27 +44,33 @@ final class UpdateTable extends StructureTable
             // Loop through the updateData columns to get the data.
             foreach ($updateData as $column => $value) {
                 if (is_array($value)) {
+                    // Method02 starts.
                     $this->actionLog['update'][] = [
                         'column' => $column,
                         'value' => $value['value'] ?? $value[0] ?? throw new Exception('Value must be defined for updating.'),
                         'type' => $value['type'] ?? $value[1] ?? 'string'
                     ];
+                    // Method02 ends.
                 } else {
+                    // Method03 starts.
                     $this->actionLog['update'][] = [
                         'column' => $column,
                         'value' => $value,
                         'type' => 'string'
                     ];
+                    // Method03 ends.
                 }
             }
         } else {
             // Loop through the updateData to get the data.
             foreach ($updateData as $column) {
+                // Method01 starts.
                 $this->actionLog['update'][] = [
                     'column' => $column['column'] ?? $column[0] ?? throw new Exception('Column must be defined for updating.'),
                     'value' => $column['value'] ?? $column[1] ?? throw new Exception('Value must be defined for updating.'),
                     'type' => $column['type'] ?? $column[2] ?? 'string'
                 ];
+                // Method01 ends.
             }
             throw new Exception('Invalid $updateData passed in update function');
         }
