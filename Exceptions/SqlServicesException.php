cb23a4f40117d947a13d78912fbb71f80bc4eb7f
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-08-19 21:34:41 +0545

    Updated the SqlServices message.

--- Exceptions/SqlServicesException.php
+++ Exceptions/SqlServicesException.php
@@ -8,7 +8,7 @@ use NGFramer\NGFramerPHPExceptions\exceptions\supportive\_BaseException;
 class SqlServicesException extends _BaseException
 {
     // Updated the values of this class.
-    protected $message = "The request is invalid.";
+    protected $message = "The request could not be completed.";
     // TODO: Change the code based on the documentation in the upcoming time.
     protected $code = 0;
     protected ?Throwable $previous = null;
