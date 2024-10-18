ce305eb1b322960c2dab4e63abaf00c0df0db3e1
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-10-18 10:23:17 +0545

    Removed errors and handlers.

--- Exceptions/SqlServicesExceptionHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace NGFramer\NGFramerPHPSQLServices\Exceptions;
-
-use NGFramer\NGFramerPHPExceptions\handlers\ApiExceptionHandler;
-
-class SqlServicesExceptionHandler extends ApiExceptionHandler
-{
-}
